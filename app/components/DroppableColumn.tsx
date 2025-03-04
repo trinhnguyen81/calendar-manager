cded621616241994edeeb8a0d412c0dda3eec871
Trinh nguyen <trinh.nguyen@smartdev.vn>
2025-03-05 01:26:44 +0700

    fix minor ui

--- app/components/DroppableColumn.tsx
+++ app/components/DroppableColumn.tsx
@@ -12,7 +12,7 @@ const DroppableColumn: React.FC<Props> = ({ id, children, isDragging }) => {
   return (
     <div ref={setNodeRef}>
         <p className="text-[#6A7988] font-semibold text-[10px] uppercase mb-2">{id}</p>
-        <div className={`group overflow-y-auto scrollbar-hidden hover:scrollbar-visible p-2 bg-[#F3F5F8] rounded-md min-h-svh ${isDragging && 'border-2 border-dashed border-[#5A57CB]'} ${isOver ? '!bg-white' : ''}`}>
+        <div className={`p-2 bg-[#F3F5F8] rounded-md min-h-svh ${isDragging && 'border-2 border-dashed border-[#5A57CB]'} ${isOver ? '!bg-white' : ''}`}>
             {children}
         </div>
     </div>
